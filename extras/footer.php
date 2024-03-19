3cbb04ee35322d10a8659757c2dcf651aef7fe46
Aditya <ricky.pathak902@gmail.com>
2024-03-19 11:48:11 +0530

    website for bloodBank

---
+++ extras/footer.php
@@ -0,0 +1,25 @@
+<footer class="p-4 bg-[rgb(17,24,39)] sm:p-6 dark:bg-gray-900">
+    <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0">
+            <a href="#" class="flex items-center">
+                <span class="self-center text-2xl text-white font-semibold whitespace-nowrap dark:text-white">APNA</span>
+            </a>
+
+        </div>
+
+    </div>
+    </div>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="" class="hover:underline">APNA</a>. All Rights Reserved.
+        </span>
+        <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+
+            </a>
+        </div>
+    </div>
+</footer>
+
+</body>
+
+</html>
---
+++ extras/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+  <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .logo {
+    padding: 1rem;
+    padding-top: 0px;
+  }
+
+  .name {
+    padding-left: 2rem;
+    font-size: 48px;
+    font-weight: bold;
+    color: #333;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  }
+
+  .contact {
+    padding: 1rem;
+    padding-right: 4rem;
+
+  }
+</style>
+
+<body>
+  <header>
+    <div class="logo">
+      <a href="index.php">
+        <i class="fa fa-hospital fa-3x"></i>
+        <span class="name">BloodBank</span>
+      </a>
+    </div>
+    <div class="contact">
+      <a href="contact_us.php">
+        <i class="fa fa-phone fa-3x" aria-hidden="true"></i>
+      </a>
+    </div>
+  </header>
+
+
+</body>
+
+</html>
